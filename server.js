f4e4280c62586243305533a13016964ef65908fe
victorximenis <victorximenis@gmail.com>
2018-08-18 17:26:09 -0300

    Heroku config

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 //Install express server
 const express = require('express');
-const path = require('path');
+path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/web'));
+app.use(express.static('./dist/web'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/web/index.html'));
+app.get('/*', (req,res) => {
+  res.sendFile(path.join(__dirname, '/dist/web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server started");
+});
