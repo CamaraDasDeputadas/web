7368b2d3ed64ce0961d83ca14a1a8527c4ae9243
victorximenis <victorximenis@gmail.com>
2018-08-19 10:56:01 -0300

    Quiz fix typo

--- src/app/pages/quiz/quiz.component.ts
+++ src/app/pages/quiz/quiz.component.ts
@@ -29,7 +29,7 @@ export class QuizComponent implements OnInit {
           {idx: 3, texto: '1989'},
           {idx: 4, texto: '2010'}
         ],
-        explanation: 'Em 1933 realizam-se as primeiras eleições em que se permite o voto às mulheres. A primeira mulher a ser eleita deputada foi a médica Carlota Pereira de Queiróz, que havia participado ativamente da Revolução Constitucionalista. Para o cargo de Presidente, a primeira candidatura registrada por uma mulher ocorreu em 1989, e apenas em 2010 uma mulher foi eleita'
+        explanation: 'Em 1933 realizam-se as primeiras eleições em que se permite o voto às mulheres. A primeira mulher a ser eleita deputada foi a médica Carlota Pereira de Queiróz, que havia participado ativamente da Revolução Constitucionalista. Para o cargo de Presidente, a primeira candidatura registrada por uma mulher ocorreu em 1989, e apenas em 2010 uma mulher foi eleita.'
       },
       {texto: 'Qual o grau de escolaridade da maioria das deputadas que compõem a Câmara?', idxResposta: 3, opcoes: [
           {idx: 1, texto: 'Ensino Médio Completo'},
@@ -37,7 +37,7 @@ export class QuizComponent implements OnInit {
           {idx: 3, texto: 'Ensino Superior Completo'},
           {idx: 4, texto: 'Pós-graduação'}
         ],
-        explanation: 'Dentre as deputadas, 60% têm Ensino Superior Completo, e 20% possuem alguma Pós-Graduação. A maioria declarou que é professora ou advogada como formação'
+        explanation: 'Dentre as deputadas, 60% têm Ensino Superior Completo, e 20% possuem alguma Pós-Graduação. A maioria declarou que é professora ou advogada como formação.'
       },
       {texto: 'Qual o percentual de mulheres na Câmara dos Deputados?', idxResposta: 1, opcoes: [
           {idx: 1, texto: '10% do total'},
@@ -45,7 +45,7 @@ export class QuizComponent implements OnInit {
           {idx: 3, texto: '30% do total'},
           {idx: 4, texto: '62% do total'}
         ],
-        explanation: 'Apesar da atualização da Lei no 9.504/1997 em 2009, obrigando os partidos a destinar 30% das candidaturas ao legislativo para mulheres, a proporção delas na câmara dos deputados ainda é muito baixa. Além de não refletir os 53% de mulheres na população brasileira, infelizmente essa obrigatoriedade fez com que a maioria das candidaturas de mulheres fossem fantasma, apenas para cumprir a cota exigida por lei'
+        explanation: 'Apesar da atualização da Lei no 9.504/1997 em 2009, obrigando os partidos a destinar 30% das candidaturas ao legislativo para mulheres, a proporção delas na câmara dos deputados ainda é muito baixa. Além de não refletir os 52% de mulheres na população brasileira, infelizmente essa obrigatoriedade fez com que a maioria das candidaturas de mulheres fossem fantasma, apenas para cumprir a cota exigida por lei.'
       },
       {texto: 'Qual a porcentagem de proposituras feitas por mulheres que chega a ser aprovada?', idxResposta: 1, opcoes: [
           {idx: 1, texto: 'Menos de 10%'},
@@ -53,7 +53,7 @@ export class QuizComponent implements OnInit {
           {idx: 3, texto: '36%'},
           {idx: 4, texto: '47%'}
         ],
-        explanation: 'No período de 1995 a 2018, das 5.504 propostas apresentadas por mulheres na Câmara, 857 foram arquivadas, o que representa 15% do total'
+        explanation: 'No período de 1995 a 2018, das 5.504 propostas apresentadas por mulheres na Câmara, 857 foram arquivadas, o que representa 15% do total.'
       },
       {texto: 'Qual a porcentagem das mulheres que se candidatam que são eleitas?', idxResposta: 1, opcoes: [
           {idx: 1, texto: '2%'},
@@ -61,7 +61,7 @@ export class QuizComponent implements OnInit {
           {idx: 3, texto: '30%'},
           {idx: 4, texto: '40%'}
         ],
-        explanation: 'Nas eleições gerais de 2014, 2583 mulheres registraram candidatura para o cargo de Deputada Federal e apenas 53 foram bem-sucedidas, o que corresponde a 2% do total'
+        explanation: 'Nas eleições gerais de 2014, 2583 mulheres registraram candidatura para o cargo de Deputada Federal e apenas 53 foram bem-sucedidas, o que corresponde a 2% do total.'
       },
       {texto: 'Qual a porcentagem de deputadas sendo investigadas por alguma irregularidade?', idxResposta: 3, opcoes: [
           {idx: 1, texto: 'Menos de 10%'},
@@ -69,7 +69,7 @@ export class QuizComponent implements OnInit {
           {idx: 3, texto: '28%'},
           {idx: 4, texto: '37%'}
         ],
-        explanation: 'Descrição: 15 das 53 mulheres estão sendo investigadas, duas delas por por ações penais (processos que podem acabar em condenação) e o restante respondem na justiça por inquéritos (procedimentos que podem resultar em processos), podendo ser incluídas na Lei da Ficha Limpa'
+        explanation: '15 das 53 mulheres estão sendo investigadas, duas delas por por ações penais (processos que podem acabar em condenação) e o restante respondem na justiça por inquéritos (procedimentos que podem resultar em processos), podendo ser incluídas na Lei da Ficha Limpa.'
       },
       {texto: 'No ranking mundial, qual a posição do Brasil em número de mulheres no Parlamento?', idxResposta: 3, opcoes: [
           {idx: 1, texto: '43º'},
@@ -77,7 +77,7 @@ export class QuizComponent implements OnInit {
           {idx: 3, texto: '153º'},
           {idx: 4, texto: '175º'}
         ],
-        explanation: 'O Brasil fica atrás, em termos de participação de mulheres em Parlamentos, de países como a Jordânia, Síria, Somália, Líbia, Marrocos, Indonésia, Iraque, Paquistão, Afeganistão, Tunísia, Emirados Árabes e mesmo a Arábia Saudita, com 19% de assentos no Congresso reservados para as mulheres'
+        explanation: 'O Brasil fica atrás, em termos de participação de mulheres em Parlamentos, de países como a Jordânia, Síria, Somália, Líbia, Marrocos, Indonésia, Iraque, Paquistão, Afeganistão, Tunísia, Emirados Árabes e mesmo a Arábia Saudita, com 19% de assentos no Congresso reservados para as mulheres.'
       },
       {texto: 'Quais dos temas abaixo estão mais presentes nas propostas das mulheres que dos homens?', idxResposta: 3, opcoes: [
           {idx: 1, texto: 'Direito da mulher e da criança'},
@@ -97,7 +97,7 @@ export class QuizComponent implements OnInit {
           {idx: 3, texto: 'Cerca de 10 por ano'},
           {idx: 4, texto: 'Cerca de 15 por ano'}
         ],
-        explanation: 'Homens e mulheres dão entrada em cerca de 5 propostas de lei por ano, mostrando que elas são tão atuantes quanto os homens'
+        explanation: 'Homens e mulheres dão entrada em cerca de 5 propostas de lei por ano, mostrando que elas são tão atuantes quanto os homens.'
       },
       {texto: 'Qual estado brasileiro tem mais deputadas eleitas?', idxResposta: 4, opcoes: [
           {idx: 1, texto: 'Alagoas'},
